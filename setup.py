47f966a9a48a04166aa7553ae142bc75680106f0
Lucas Krupinski <lucasjkr@me.com>
2025-09-18 13:20:05 -0400

    Bump version number

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='microsoft_graph_helpers',
-    version='0.1.0',
+    version='0.1.1',
     description='Helper functions for interacting with Microsoft Graph API',
     author='Lucas Krupinski',
     packages=find_packages(),
