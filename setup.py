db87f5b66373174508c403c8f399cc97b8a3d388
Lucas Krupinski <lucasjkr@me.com>
2025-09-19 13:41:34 -0400

    bump version to 0.1.2
    reduce minimum python3 requirement from 3.12 to 3.10

--- setup.py
+++ setup.py
@@ -2,12 +2,12 @@ from setuptools import setup, find_packages
 
 setup(
     name='microsoft_graph_helpers',
-    version='0.1.1',
+    version='0.1.2',
     description='Helper functions for interacting with Microsoft Graph API',
     author='Lucas Krupinski',
     packages=find_packages(),
     install_requires=[
         'requests>=2.32.5'
     ],
-    python_requires='>=3.12',
+    python_requires='>=3.10',
 )
